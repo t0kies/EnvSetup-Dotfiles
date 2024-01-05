32128c2c848bce4f95c77302d63de7c5e36f4edb
T.A <83424097+t0kies@users.noreply.github.com>
2024-01-05 02:07:39 -1000

    Update print.sh

--- lib/print.sh
+++ lib/print.sh
@@ -2,5 +2,5 @@
 # print.sh
 
 # Fetch the content of text.txt and echo it
-TEXT_CONTENT=$(curl -s https://raw.githubusercontent.com/t0kies/EnvSetup-Dotfiles/assets/text.txt)
+TEXT_CONTENT=$(curl -s https://raw.githubusercontent.com/t0kies/EnvSetup-Dotfiles/main/assets/text.txt)
 echo "$TEXT_CONTENT"
