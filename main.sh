6c90eba1ad52b9c2651979ab786205349c14715a
T.A <83424097+t0kies@users.noreply.github.com>
2024-01-05 02:07:58 -1000

    Update main.sh

--- main.sh
+++ main.sh
@@ -2,4 +2,4 @@
 # main.sh
 
 # Fetch and run the print.sh script
-source <(curl -s https://raw.githubusercontent.com/t0kies/EnvSetup-Dotfiles/lib/print.sh)
+source <(curl -s https://raw.githubusercontent.com/t0kies/EnvSetup-Dotfiles/main/lib/print.sh)
